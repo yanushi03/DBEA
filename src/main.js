cf0b0ec63d92cd34c5145205eec77b82f0dbede6
RebeccaSCIS <rebeccangoh.2023@scis.smu.edu.sg>
2025-11-02 15:47:38 +0800

    Include formatDate function in main.js

--- src/main.js
+++ src/main.js
@@ -2,6 +2,21 @@ import Vue from 'vue';
 import App from './App.vue';
 import router from './router/index.js';
 
+
+export function formatDate(dateTime) {
+  const date = new Date(dateTime);
+
+  const options = {
+    day: '2-digit',
+    month: 'short',
+    year: 'numeric',
+    hour: '2-digit',
+    minute: '2-digit',
+    hour12: true
+  }
+  return date.toLocaleDateString('en-US', options)
+}
+
 new Vue({
     router,
     render: h => h(App),
