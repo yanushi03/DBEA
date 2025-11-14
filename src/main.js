eeac04a174b25bd547e6f51469e0d98adeb27539
jaslyn <jaslyn.yap.2023@scis.smu.edu.sg>
2025-11-14 22:35:31 +0800

    npm install vue toastification

--- src/main.js
+++ src/main.js
@@ -2,9 +2,24 @@ import Vue from 'vue';
 import App from './App.vue';
 import router from './router/index.js';
 import '@fortawesome/fontawesome-free/css/all.css';
+import Toast from "vue-toastification"
+import "vue-toastification/dist/index.css"
 
 
-
+Vue.use(Toast, {
+  position: "top-right",
+  timeout: 3000,
+  closeOnClick: true,
+  pauseOnFocusLoss: true,
+  pauseOnHover: true,
+  draggable: true,
+  draggablePercent: 0.6,
+  showCloseButtonOnHover: false,
+  hideProgressBar: false,
+  closeButton: "button",
+  icon: true,
+  rtl: false,
+});
 
 new Vue({
     router,
